f91a7010f10cf6a10529e0d44e500a5fda476950
Ian <ianx021@gmail.com>
2021-10-28 14:57:43 -0300

    update table styling

--- client/src/components/StudentsTable.js
+++ client/src/components/StudentsTable.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import { Table } from 'antd'
+
+const columns = [
+  {
+    title: 'Nome',
+    dataIndex: 'nome',
+    key: 'nome',
+  },
+  {
+    title: 'CPF',
+    dataIndex: 'cpf',
+    key: 'cpf',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+]
 
 const StudentsTable = ({ loading, error, data }) => {
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  return data.users.map(({ id, nome, email, cpf }) => (
-    <div key={id}>
-      <p>
-        {nome} - {email} - {cpf}
-      </p>
-    </div>
-  ))
+  const dataSource = data.users
+
+  return (
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+      rowKey="id"
+      style={{ width: '80vw' }}
+    />
+  )
 }
 
 export default StudentsTable
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import StudentsTable from '../components/StudentsTable'
+
 import { useQuery } from '@apollo/client'
 import { USERS } from '../services/apollo/queries'
+import { Space } from 'antd'
 
 const Home = () => {
   const { loading, error, data } = useQuery(USERS)
 
   return (
     <div className="flex-container">
-      <h2>Students</h2>
-      <StudentsTable loading={loading} error={error} data={data} />
+      <Space style={{ flexDirection: 'column' }}>
+        <h2>Students</h2>
+
+        <StudentsTable loading={loading} error={error} data={data} />
+      </Space>
     </div>
   )
 }
