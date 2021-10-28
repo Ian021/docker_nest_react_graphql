c9e4872c24a0eac4896359486b70a6a26bae47dc
Ian <ianx021@gmail.com>
2021-10-28 20:23:52 -0300

    implement filtering on backend

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,10 +2,34 @@ const models = require('./models')
 
 module.exports = {
   Query: {
-    users: () => models.student.findAll(),
+    getUsers: (_, { nome, cpf, email }) => {
+      const filters = {}
+
+      if (nome != undefined && nome != '') {
+        filters.nome = { [models.Sequelize.Op.substring]: nome }
+      }
+
+      if (cpf != undefined && cpf != 0) {
+        filters.cpf = { [models.Sequelize.Op.substring]: cpf }
+      }
+
+      if (email != undefined && email != '') {
+        filters.email = { [models.Sequelize.Op.substring]: email }
+      }
+
+      return models.student.findAll({
+        where: filters,
+      })
+    },
   },
   Mutation: {
     createUser: (_, { nome, cpf, email }) =>
       models.student.create({ nome, cpf, email }),
+    deleteUser: (_, { id }) =>
+      models.student.destroy({
+        where: {
+          id,
+        },
+      }),
   },
 }
